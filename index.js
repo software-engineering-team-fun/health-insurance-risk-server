2339753a2c84f0fcc1454882103cf13e458e974d
Anthony Mastores <mastores27@gmail.com>
2022-10-13 08:32:39 -0500

    working skeleton

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 app = express()
 
+var url = require('url');
 app.use(express.json());
-
 const cors = require('cors')
 app.use(
 	cors({
@@ -11,9 +11,19 @@ app.use(
 	})
 )
 
+const port = process.env.PORT || 3000
+
+// Use Express to publish static HTML, CSS, and JavaScript files that run in the browser. 
+app.use(express.static(__dirname + '/static'))
 let bmi;
 
-//from node.js azure template
+app.get('/send', (req, res) => { 	// localhost:3000/send for local testing, https://health-insurance-risk-server.azurewebsites.net/send for azure server -am
+	var inputs = url.parse(req.url, true).query
+	const age = parseInt(inputs.patAge)
+   res.send('Patient Age ' + age)
+
+});
+//from node.js azure template -am
 app.get('/calculate-bmi', (request, response) => {
 	console.log('Calling "/calculate-bmi" on the Node.js server.')
 	var inputs = url.parse(request.url, true).query
@@ -30,19 +40,7 @@ app.get('/calculate-bmi', (request, response) => {
 	response.type('text/plain')
 	response.send('Todo: Implement "/calculate-bmi"')
 })
-app.post('https://health-insurance-risk-server.azurewebsites.net/send', (req, res) => {
-   let age =  req.body.patAge;
-   let height = req.body.patHeight;
-   let weight = req.body.patWeight;
-   //let bmi = calcBMI();
-   let sys = req.body.patSys;
-   let dia = req.body.patDia;
-   let diaH = req.body.patDiaH;
-   let cancerH = req.body.patCancerH;
-   let AlzH = req.body.patAlzH;
-   res.send( age, height,weight,sys,diaH,cancerH,AlzH);
 
-});
 // Custom 404 page.
 app.use((request, response) => {
 	response.type('text/plain')
@@ -57,5 +55,9 @@ app.use((request, response) => {
 	response.status(500)
 	response.send('500 - Server Error')
   })
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`))
+
+  app.listen(port, () => console.log(
+	`Express started at \"http://localhost:${port}\"\n` +
+	`press Ctrl-C to terminate.`)
+  )
+  
